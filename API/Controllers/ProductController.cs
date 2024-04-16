20f1112588d5c6258a7ff9aa4848a9d1c10ac9c7
Ahmed Abdelnaser <ahmedabdelnaser770@gmail.com>
2024-04-16 14:29:02 +0200

    intial

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult GetAllProducts()
+        {
+            return Ok("all Products");
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,18 @@
+
+
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//AddCors
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("StoreDB"))
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
