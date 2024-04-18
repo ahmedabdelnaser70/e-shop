a1ec69ebfd9ab8dc4acfe10cb9bc1281758295e9
Ahmed Abdelnaser <ahmedabdelnaser770@gmail.com>
2024-04-18 11:04:35 +0200

    first controller

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -7,15 +10,28 @@ namespace API.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        public ProductController()
+        private readonly StoreContext _context;
+        public ProductController(StoreContext context)
         {
-
+            _context = context;
         }
 
         [HttpGet]
-        public IActionResult GetAllProducts()
+        public async Task<ActionResult<List<Product>>> GetAllProducts()
         {
-            return Ok("all Products is here");
+            var Products = await _context.Products.ToListAsync();
+            return Products;
         }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductID(int id)
+        {
+            var Product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            return Product;
+        }
+
     }
 }
