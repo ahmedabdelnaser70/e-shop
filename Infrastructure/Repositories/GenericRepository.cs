301b86cabb1923e0440980ff7d696a5b8373eec2
Ahmed Abdelnaser <ahmedabdelnaser770@gmail.com>
2024-06-03 16:53:18 +0300

    Specification_Pattern

--- Infrastructure/Repositories/GenericRepository.cs
+++ Infrastructure/Repositories/GenericRepository.cs
@@ -1,10 +1,12 @@
 ﻿using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,6 +27,22 @@ namespace Infrastructure.Repositories
         public async Task<IReadOnlyList<T>> GetListAsync()
         {
             return await _context.Set<T>().ToListAsync();
+        }
+
+        // Specificatios --------------------------------------------
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
     }
 }
