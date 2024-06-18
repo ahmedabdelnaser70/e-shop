6f54f5ad5b82c9300f8df22d82e99b9ac1b756bf
Ahmed Abdelnaser <ahmedabdelnaser770@gmail.com>
2024-06-18 20:02:39 +0300

    core&sharedModule

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +7,8 @@ import { Pagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'e-Shop';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http
-      .get<Pagination<Product[]>>(
-        'https://localhost:7241/api/Product?pagesize=40'
-      )
-      .subscribe({
-        next: (response) => (this.products = response.data),
-        error: (error) => console.log(error),
-        complete: () => {
-          console.log('request complete');
-        },
-      });
-  }
+  ngOnInit(): void {}
 }
