6a5dd8ff5891ade0dc99c8538e40f6b6c79795df
Ahmed Abdelnaser <ahmedabdelnaser770@gmail.com>
2024-06-26 14:09:21 +0300

    routes-lazyLoading

--- Client/src/app/shop/product-details/product-details.component.ts
+++ Client/src/app/shop/product-details/product-details.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShopService } from '../shop.service';
+import { Product } from 'src/app/shared/models/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  productById?: Product;
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id)
+      this.shopService.getProductById(+id).subscribe({
+        next: (product) => (this.productById = product),
+        error: (error) => console.log(error),
+      });
+  }
 }
